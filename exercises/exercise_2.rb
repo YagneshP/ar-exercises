5ba7ebd76e1444400eedc0cab825455ecb12dc1f
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-03 20:06:35 -0500

    Find store with find_by method

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,14 @@
 require_relative '../setup'
 require_relative './exercise_1'
-
+require 'pp'
 puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+pp  @store1
+@store2 = Store.find_by(id: 2)
+pp @store2
+
+@store1.name = 'Express'
+@store1.save
