87f10f1af5a89fdb9c7ccdaa769205ac2aba96fa
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-04 11:20:06 -0500

    Displayong error while creating store with blank info

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -5,8 +5,18 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
+require 'pp'
 
 puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+@store_name = gets.chomp
+@new_store = Store.create(name: @store_name)
+puts "error: #{@new_store.errors.size}"
+
+if(@new_store.errors.size > 0) 
+  @new_store.errors.full_messages.each do |error|
+    puts "#{error}"
+  end
+end
