3f69ac141c05ae0920ba12a49ed232a9ae6b98a5
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-03 23:06:26 -0500

    Sum to calculate annual revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+puts "Total Revenue : #{@total_revenue}"
+@average_revenue = @total_revenue / Store.count
+puts "Average Revenue : #{@average_revenue}"
+
+puts Store.where("annual_revenue > ?", 1000000).size
