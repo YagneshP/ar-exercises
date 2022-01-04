cf923317650bb6aec926ab78f5fe32314fc97d72
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-04 11:18:41 -0500

    change condition for checking mens appareal and womens appareal

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,11 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validate :must_have_an_apparel, on: :create
+  def must_have_an_apparel
+    if !(mens_apparel || womens_apparel)
+      errors.add(:base, 'At least an appreal should be true' )
+    end
+  end
 end
