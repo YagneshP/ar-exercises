5894f21b177cc1fddf79e43c844bba65c558eb6a
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-03 23:04:14 -0500

    Use where to find collection of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -2,8 +2,22 @@ require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
-
+require 'pp'
 puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: 'Surrey', annual_revenue: 224000, womens_apparel: true)
+Store.create(name: 'Whistler', annual_revenue: 1900000,mens_apparel: true)
+Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_apparel = Store.where(mens_apparel: true)
+
+@mens_apparel.each do |store|
+  puts "Name : #{store.name}"
+  puts "Revenue : #{store.annual_revenue}"
+end
+
+@womens_apparel = Store.where(["womens_apparel = ? and annual_revenue < ?", "true", "1000000"])
+
+# pp @womens_apparel
